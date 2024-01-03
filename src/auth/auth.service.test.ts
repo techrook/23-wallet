6e13545ece0c6a4d89f4512dd8eae4f56e530966
techrook <mondayitohowo3@gmail.com>
2024-01-03 13:47:00 +0100

    auth test passes

--- src/auth/auth.service.test.ts
+++ src/auth/auth.service.test.ts
@@ -5,6 +5,7 @@ import { ConfigService } from "@nestjs/config";
 import { PrismaService } from "../prisma/prisma.service";
 import { WalletService } from '../wallet/wallet.service';
 import { TransactionService } from '../transaction/transaction.service';
+import { CreateUserDto, LoginUserDto } from './auth.dto';
 describe('The AuthenticationService', () => {
     let authService: AuthService;
     beforeEach(async () => {
@@ -24,5 +25,38 @@ describe('The AuthenticationService', () => {
 
     it('should be defined', () => {
         expect(authService).toBeDefined();
+      });
+
+      it('should create a new user and return a verification token', async () => {
+        const createUserDto: CreateUserDto = {
+            email: 'test@example.com',
+            username: 'testuser',
+            password: 'password',
+            passwordConfirm: 'differentpassword',
+            phone: '123456789',
+        };
+        const verificationToken = await authService.signup(createUserDto);
+
+        expect(typeof verificationToken).toBe('object');
+      });
+
+      it('should login user and return a verification token', async () => {
+        const logInUser: LoginUserDto = {
+            email: 'test@example.com',
+            password: 'password'
+        };
+        const verificationToken = await authService.login(logInUser);
+
+        expect(typeof verificationToken).toBe('object');
+      });
+
+      it('should return an access token with the correct payload', async () => {
+        const userId = 1;
+        const userEmail = 'test@example.com';
+  
+        const token = await authService.signToken(userId, userEmail);
+  
+        expect(typeof token).toBe('object');
+        
       });
 })
